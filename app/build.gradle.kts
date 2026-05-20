1d1c5bf5041426d9a03389350fb681f91d2db8ea
Matthew Mumbere <matthewmumbere@gmail.com>
2026-05-20 21:35:38 +0300

    chore: add app module Gradle build file

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "cfd.spookymallard.money"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "cfd.spookymallard.money"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isDebuggable = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = false
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
---
+++ app/proguard-rules.pro
@@ -0,0 +1,4 @@
+-keepclassmembers class cfd.spookymallard.money.** {
+    @android.webkit.JavascriptInterface <methods>;
+}
+-keep public class cfd.spookymallard.money.MainActivity
