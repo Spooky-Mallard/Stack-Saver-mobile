32173be273b16357a6f4327ec95b474b592cbcf0
Spooky Mallard <matthewmumbere@gmail.com>
2026-05-21 08:19:45 +0300

    build changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "cfd.spookymallard.money"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "cfd.spookymallard.money"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0.0"
     }
@@ -50,6 +50,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 }
